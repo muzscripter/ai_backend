e4c0b67139b3a456f93b6d638608a2100784f555
graverealm <109730545+muzscripter@users.noreply.github.com>
2024-06-03 10:01:19 -0400

    Update civilianbdata.lua

--- data/behaviortrees/civilianbdata.lua
+++ data/behaviortrees/civilianbdata.lua
@@ -1,3 +1,13 @@
-local CivilianBehaviorData = {};
+local CivilianBehaviorData = {
+  Idle  =  {
+    Conditions = {'_OUT_OF_COMBAT', 'WANDERING'};
+    Transitions = {
+      ['Primary'] = 'Wandering',
+      ['_IN_COMBAT'] = 'RunningAway'
+    };
+    ActionDirectory = game.ServerStorage.Modules.Functionality.Actions.Civilians;
+    ActionName = 'Idle';
+  };
+};
 
 return CivilianBehaviorData;
